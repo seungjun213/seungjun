7b7faae4f1f4a200a0dc4c80c80b033451605d9f
seungjun213 <sngjun128@naver.com>
2023-11-01 11:51:12 +0900

    웰컴을 asdf 로 바꿈

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and asdf!");
 
         }
     }
